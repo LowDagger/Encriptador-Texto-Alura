d769c9874f44902ff690d74537f1bdf76ef674c7
LowDagger <25236897+LowDagger@users.noreply.github.com>
2024-03-20 02:31:25 -0400

    proyecto final

---
+++ index.js
@@ -0,0 +1,66 @@
+function encriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muneco = document.getElementById("muneco");
+
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    parrafo.textContent = "";
+    muneco.src = "./img/encriptado.jpg";
+    document.getElementById("copy-button").innerHTML = '<i class="fas fa-copy"></i>';
+  } else {
+    muneco.src = "./img/muneco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("Ooops!", "Debes ingresar un texto", "warning");
+  }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muneco = document.getElementById("muneco");
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto desencriptado con éxito";
+    parrafo.textContent = "";
+    muneco.src = "./img/desencriptado.jpg";
+    document.getElementById("copy-button").innerHTML = '<i class="fas fa-copy"></i>';
+  } else {
+    muneco.src = "./img/muneco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("Ooops!", "Debes ingresar un texto", "warning");
+  }
+}
+
+document.getElementById("copy-button").addEventListener("click", function() {
+  const textArea = document.getElementById("texto");
+  const textToCopy = textArea.value;
+
+  navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      alert("Texto copiado al portapapeles");
+    })
+    .catch((err) => {
+      console.error("Failed to copy text: ", err);
+    });
+});
